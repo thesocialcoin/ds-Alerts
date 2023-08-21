b782f1d54e770e81f64de180f3d87ee314a61e07
Oscar D <74262517+oscar-delgado@users.noreply.github.com>
2023-08-21 09:38:57 +0200

    📖 Miscelaneous changes for better readability (#1)

--- src/ds_alerts/anomaly.py
+++ src/ds_alerts/anomaly.py
@@ -1,23 +1,26 @@
+# ####################### Code Implemented by Alejandro Bonell ########################
+# Inspired by: Anomaly detection in univariate time series incorporating active learning
+# Link: https://www.sciencedirect.com/science/article/pii/S2772415822000323
+
 import math
-import pandas as pd
 import copy
+
+import pandas as pd
 import numpy as np
-import sys
 
-from .time_series import timeSeries
-
-sys.path.append("/Users/alejandrobonell/ds-Alerts")
+from .time_series import TimeSeries
 
 
-class algorithmAnomalyTimeSeries:
+class AnomalyDetector:
     """
-    A subclass of Time Series more focused on the anomaly properties
-    of the time series
+    A class associated to the TimeSeries class more focused on the anomaly properties of it
     """
+
+    WINDOW_SIZE = 30
 
     def __init__(
         self,
-        window: int,
+        window: int = WINDOW_SIZE,
         threshold: float = 10.0,
         a: float = 0.5,
         multiplier: float = 1.96,
@@ -28,7 +31,7 @@ class algorithmAnomalyTimeSeries:
         self.multiplier = multiplier
         self.window = window
 
-    def detect_alerts(self, time_series: timeSeries):
+    def detect_alerts(self, time_series: TimeSeries):
         """
         Method to detect the alerts of the whole time series!
         """
@@ -36,11 +39,11 @@ class algorithmAnomalyTimeSeries:
         modified_time_series = copy.deepcopy(time_series)
         alerts_idx = []
         alerts = pd.Series(
-            [np.nan] * len(time_series.time_series), index=time_series.time_series.index
+            [np.nan] * len(time_series.data), index=time_series.data.index
         )
-        threshold = max(self.threshold, time_series.time_series.median())
+        threshold = max(self.threshold, time_series.data.median())
 
-        for i in range(len(modified_time_series.time_series)):
+        for i in range(len(modified_time_series.data)):
             # FIRST PART: Compute the upper bound & lower bound
             modified_time_series.compute_trend(self.window)
             modified_time_series.compute_std(self.window)
@@ -58,22 +61,22 @@ class algorithmAnomalyTimeSeries:
 
             # SECOND PART: Detect the alert
             if (
-                modified_time_series.time_series[i] > upper_bound[i]
-                and modified_time_series.time_series[i] > threshold
+                modified_time_series.data[i] > upper_bound[i]
+                and modified_time_series.data[i] > threshold
             ):
                 # THIRD PART: Store the alert
                 alerts_idx.append(i)
-                alerts[i] = time_series.time_series[i]
+                alerts[i] = time_series.data[i]
 
                 # FOURTH PART a): UPDATE the time series
-                modified_time_series.time_series[i] = modified_time_series.time_series[
-                    i
-                ] - self.a * (modified_time_series.time_series[i] - upper_bound[i])
+                modified_time_series.data[i] = modified_time_series.data[i] - self.a * (
+                    modified_time_series.data[i] - upper_bound[i]
+                )
 
-            if modified_time_series.time_series[i] < lower_bound[i]:
+            if modified_time_series.data[i] < lower_bound[i]:
                 # FOURTH PART b) : UPDATE the time series
-                modified_time_series.time_series[i] = modified_time_series.time_series[
-                    i
-                ] + self.a * (lower_bound[i] - modified_time_series.time_series[i])
+                modified_time_series.data[i] = modified_time_series.data[i] + self.a * (
+                    lower_bound[i] - modified_time_series.data[i]
+                )
 
         return alerts
--- src/ds_alerts/time_series.py
+++ src/ds_alerts/time_series.py
@@ -1,46 +1,23 @@
-import sys
-import os
-import pandas as pd
-from typing import List
-
-# import math
-
-# import numpy as np
-# from statsmodels.tsa.seasonal import seasonal_decompose
-
-# from src.conf import (WINDOW_SIZE)
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-
-sys.path.append(os.getcwd())
-
 # ####################### Code Implemented by Alejandro Bonell ########################
 # Inspired by: Anomaly detection in univariate time series incorporating active learning
 # Link: https://www.sciencedirect.com/science/article/pii/S2772415822000323
-# Version 0.1
+
+from typing import List
+
+import pandas as pd
 
 
-# ########################### TimeSeries ############################
-
-
-class timeSeries:
+class TimeSeries:
     """
-    A class to represent a Time Series and to calculate some typical properties of
-    Time series
+    Class to represent a time series and to calculate some typical properties of it
     """
-
-    WINDOW_SIZE_STD = 20
-    WINDOW_SIZE_MEAN = 20
 
     def __init__(self, ts: List[int], dates: List[str]):
         """
         Initializes the TS with raw time data values and its corresponding dates
-        Args:
-            - ts, numpy array: the TS values
-            - dates, numpy array: the corresponding dates
         """
 
-        self.time_series = pd.Series(ts, index=pd.to_datetime(dates))
+        self.data = pd.Series(ts, index=pd.to_datetime(dates))
 
         self.trend = None
         self.std = None
@@ -52,20 +29,17 @@ class timeSeries:
         """
         Computes the trend of the time series
         """
-        self.trend = self.time_series.rolling(window=window, closed="left").mean()
+        self.trend = self.data.rolling(window=window, closed="left").mean()
 
     def compute_std(self, window: int):
         """
         Computes the standard deviation of the time series
         """
-        self.std = self.time_series.rolling(window=window, closed="left").std()
+        self.std = self.data.rolling(window=window, closed="left").std()
 
     def prediction_interval(self, window: int, multiplier: float = 1.96):
         """
         Calculates the prediction intervals for the TS or the anomaly adjusted TS
-
-        Args:
-        anomaly (bool): If True compute prediction intervals for the anomaly adjusted TS
         """
 
         if self.trend is None:
