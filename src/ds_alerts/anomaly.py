d8411d2781db49e31b7667d0055a1c632a2fc39a
Óscar Delgado <odelgado@citibeats.com>
2023-08-22 17:19:23 +0200

    Refactor AnomalyDetector

--- src/ds_alerts/anomaly.py
+++ src/ds_alerts/anomaly.py
@@ -2,11 +2,8 @@
 # Inspired by: Anomaly detection in univariate time series incorporating active learning
 # Link: https://www.sciencedirect.com/science/article/pii/S2772415822000323
 
-import math
 import copy
-
-import pandas as pd
-import numpy as np
+from typing import Dict, List
 
 from .time_series import TimeSeries
 
@@ -16,11 +13,11 @@ class AnomalyDetector:
     A class associated to the TimeSeries class more focused on the anomaly properties of it
     """
 
-    WINDOW_SIZE = 30
+    WINDOW = 30
 
     def __init__(
         self,
-        window: int = WINDOW_SIZE,
+        window: int = WINDOW,
         threshold: float = 10.0,
         a: float = 0.5,
         multiplier: float = 1.96,
@@ -36,41 +33,26 @@ class AnomalyDetector:
         Method to detect the alerts of the whole time series!
         """
 
-        modified_time_series = copy.deepcopy(time_series)
-        alerts_idx = []
-        alerts = pd.Series(
-            [np.nan] * len(time_series.data), index=time_series.data.index
-        )
+        mod_ts = copy.deepcopy(time_series)
+        alerts = []
         threshold = max(self.threshold, time_series.data.median())
 
         # FIRST PART: Compute the upper bound & lower bound
-        pred_interval = modified_time_series.prediction_interval(self.window)
+        pred_interval = mod_ts.prediction_interval(self.window)
         upper_bound = pred_interval["upper_bound"]
         lower_bound = pred_interval["lower_bound"]
 
-        for i in range(len(modified_time_series.data)):
-            # Let's check the confidence intervals are available
-            if i == 0 or math.isnan(upper_bound[i]):
-                continue
-
+        for i, item in enumerate(mod_ts.data[self.window :], self.window):
             # SECOND PART: Detect the alert
-            if (
-                modified_time_series.data[i] > upper_bound[i]
-                and modified_time_series.data[i] > threshold
-            ):
+            if item > upper_bound[i] and item > threshold:
                 # THIRD PART: Store the alert
-                alerts_idx.append(i)
-                alerts[i] = time_series.data[i]
+                alerts.append({item.index: item})
 
                 # FOURTH PART a): UPDATE the time series
-                modified_time_series.data[i] = modified_time_series.data[i] - self.a * (
-                    modified_time_series.data[i] - upper_bound[i]
-                )
+                item = item - self.a * (item - upper_bound[i])
 
-            if modified_time_series.data[i] < lower_bound[i]:
+            if item < lower_bound[i]:
                 # FOURTH PART b) : UPDATE the time series
-                modified_time_series.data[i] = modified_time_series.data[i] + self.a * (
-                    lower_bound[i] - modified_time_series.data[i]
-                )
+                item = item + self.a * (lower_bound[i] - item)
 
         return alerts
