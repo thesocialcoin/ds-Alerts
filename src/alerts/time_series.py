4f76f56f7bdb5273a346238aed38b8eb6c0d2fd3
wilberquito <wbq.software@gmail.com>
2023-11-16 17:36:55 +0100

    feat: working on taking singals from transformed data

--- src/alerts/time_series.py
+++ src/alerts/time_series.py
@@ -3,12 +3,110 @@
 # active learning.
 # https://www.sciencedirect.com/science/article/pii/S2772415822000323
 
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class AnomalyTS():
+
+    QUANTILE = 1.625
+    WINDOW = 30
+
+    def __init__(self,
+                 T: List[Tuple[Any, int]],
+                 parameters: Dict[str, Any]):
+        """Time series anomaly
+
+        Args:
+            T (List[Tuple[int, int]]): represents data point containing a
+        timestamp and an associating value.
+            parameters (Dict[str, Any]): set of hyper-parameters that help to
+                compute the prediction intervals.
+        """
+
+        self.dates, self.values = zip(*T)
+        self.window = (
+            parameters['window'] if 'window' in parameters else self.WINDOW
+        )
+
+    def normalize_time_series(self) -> List[Tuple[Any, int]]:
+        normalized_values = self._flatten_transform(self.values)
+        return list(zip(self.dates, normalized_values))
+
+    def _flatten_transform(self, values: List[int]) -> List[int]:
+        np_values = np.array(values)
+        flatten_values = np.sqrt(np_values)
+        return flatten_values
+
+    def _inverse_transform(self, values: List[int]) -> List[int]:
+        np_values = np.array(values)
+        inverse_values = np.square(np_values)
+        return inverse_values
+
+    def prediction_interval(self) -> Dict[str, Any]:
+        prediction_interval = {
+            "dates": [date for date in self.dates[:self.window]],
+            "values": [value for value in self.values[:self.window]],
+            "lb": [None for _ in self.dates[:self.window]],
+            "ub": [None for _ in self.dates[:self.window]],
+            "flatten_values": [
+                self._flatten_transform(value)
+                for value in self.values[:self.window]
+            ],
+            "flatten_lb": [None for _ in self.dates[:self.window]],
+            "flatten_ub": [None for _ in self.dates[:self.window]],
+        }
+
+        post_window_dates = self.dates[self.window:]
+        post_window_values = self.values[self.window:]
+        targets = enumerate(zip(post_window_dates, post_window_values))
+
+        for i, (date, value) in targets:
+            # Calculate statistic values for this window
+
+            pre_window_values = self.values[i: self.window + i]
+            flatten_pre_window_values = self. \
+                _flatten_transform(pre_window_values)
+
+            flatten_value = self._flatten_transform([value])[0]
+            flatten_mean_value = np.mean(flatten_pre_window_values)
+            flatten_std_value = np.std(flatten_pre_window_values)
+
+            mean_value = np.mean(pre_window_values)
+            std_value = np.std(pre_window_values)
+
+            flatten_lb = flatten_mean_value - self.QUANTILE * flatten_std_value
+            flatten_ub = flatten_mean_value + self.QUANTILE * flatten_std_value
+
+            lb = mean_value - self.QUANTILE * std_value
+            ub = mean_value + self.QUANTILE * std_value
+
+            if lb and lb < 0:
+                lb = 0.0
+
+            if flatten_lb and flatten_lb < 0:
+                flatten_lb = 0.0
+
+            prediction_interval["dates"].append(date)
+            prediction_interval["values"].append(value)
+            prediction_interval["lb"].append(lb)
+            prediction_interval["ub"].append(ub)
+            prediction_interval["flatten_values"].append(flatten_value)
+            prediction_interval["flatten_lb"].append(flatten_lb)
+            prediction_interval["flatten_ub"].append(flatten_ub)
+
+        return prediction_interval
+
+    def plot(self) -> None:
+        plt.subplots(figsize=(12, 5))
+        plt.plot(self.dates, self.values)
 
 
 class TimeSeries:
     """
-    Class to represent a time series and to calculate some typical properties of it
+    Class to represent a time series and to calculate some
+    typical properties of it
     """
 
     # Algorithm hiperparameters
@@ -45,7 +143,7 @@ class TimeSeries:
             # Calculate bounds
             upper_bound = median + (multiplier * 1.4826 * mad)
             lower_bound = median - (multiplier * 1.4826 * mad)
-            
+
             if lower_bound and lower_bound < 0:
                 lower_bound = 0.0
 
