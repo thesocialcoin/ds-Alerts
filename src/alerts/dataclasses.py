2187e8ea3b8e97baba4696d3c3d8fc91ff0005df
Wil <54361466+wilberquito@users.noreply.github.com>
2023-11-30 16:28:28 +0100

    Refactor code ds-alerts (#9)
    * wip: working on refactor the hole code
    
    * fix: Adapt source code to types
    
    * ref: MedianTS detector
    
    This commit contains the execution of a notebook that uses the new API
    
    * Update dataclasses.py

---
+++ src/alerts/dataclasses.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class LimitInterval:
+    def __init__(self, lb: float, ub: float):
+        self.lower_bound = lb
+        self.upper_bound = ub
+
+
+@dataclass
+class PredictionInterval:
+    def __init__(self, t, v: float, lb: float, ub: float):
+        self.time = t
+        self.value = v
+        self.lower_bound = lb
+        self.upper_bound = ub
+
+
+@dataclass
+class Event:
+    def __init__(self, t, v: float):
+        self.time = t
+        self.value = v
+
+
+@dataclass
+class TimeSeries:
+    def __init__(self, events: List[Event]):
+        self.events = events
