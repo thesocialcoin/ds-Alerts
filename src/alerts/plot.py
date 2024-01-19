465d48026a11dc8f5fea37db753d676293612349
wilberquito <wbq.software@gmail.com>
2024-01-19 16:32:44 +0100

    fix: hot fixes segmentation

--- src/alerts/plot.py
+++ src/alerts/plot.py
@@ -50,32 +50,43 @@ def plot_groups(
     ax.plot(df["date"], df["lower_bound"], label="lower_bound")
     ax.plot(df["date"], df["upper_bound"], label="upper_bound")
 
+    dates, group_events = None, None
+
     if show_anomalies:
         detector = AnomalyDetector()
-        events = detector.detect_alerts_groups(ts)
+        group_events = detector.detect_alerts_groups(ts)
 
-        x = [evs.date for e in events for evs in e]
-        y = [evs.value for e in events for evs in e]
+        dates = [evs.date for e in group_events for evs in e]
+        values = [evs.value for e in group_events for evs in e]
 
         start_idx = 0
-        for evs in events:
+        for evs in group_events:
             length = len(evs)
             end_idx = start_idx + length
 
             ax.scatter(
-                x[start_idx:end_idx],
-                y[start_idx:end_idx],
+                dates[start_idx:end_idx],
+                values[start_idx:end_idx],
                 marker="o",
                 alpha=0.5,
                 color="red",
             )
-            ax.axvspan(x[start_idx], x[end_idx - 1], color="blue", alpha=0.2, lw=0)
+            ax.axvspan(
+                dates[start_idx], dates[end_idx - 1], color="blue", alpha=0.2, lw=0
+            )
 
             start_idx = end_idx
 
+    n_events = len(dates) if dates else 0
+    n_group_events = (
+        len([group for group in group_events if len(group) > 1]) if group_events else 0
+    )
+    extra_title = f" - Anomalies: {n_events}, Groups: {n_group_events}"
+    title = title + extra_title if show_anomalies else ""
+
     ax.set_xlabel("Days")
     ax.set_ylabel("Magnitude")
-    ax.set_title(title + (" - Anomalies {}".format(len(x)) if show_anomalies else ""))
+    ax.set_title(title)
 
     ax.legend()
     plt.show()
